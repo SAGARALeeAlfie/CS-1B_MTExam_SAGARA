8962d0d624e4f7721ee3c5988a4750c181cdd431
SAGARA,Lee Alfie A. <leealfiesagara03@gmail.com>
2025-03-21 18:10:08 +0800

    Create UserProfile.js

---
+++ UserProfile.js
@@ -0,0 +1,12 @@
+//cllect user information
+let fullName = prompt("Enter your full name (first and last):");
+let age = prompt("Enter your age:");
+let favoriteNumber = prompt("Enter your favorite number:");
+let favoriteColor = prompt("Enter your favorite color:");
+
+//log the collected information to the console
+console.log("User Information:");
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
